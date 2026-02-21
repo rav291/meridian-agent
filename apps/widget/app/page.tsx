5a5924e3a30683504117e42e1a4b5fe7da76c887
Ravi Anand <ravianand291@gmail.com>
2026-02-22 00:42:36 +0530

    feat: initial mono repo setup

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,15 @@
-import { add } from "@workspace/math/add"
 import { Button } from "@workspace/ui/components/button"
+import { add } from "../../../packages/math/src/add"
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello Web</h1>
+        <h1 className="text-2xl font-bold">Hello Widget</h1>
         <div className="flex gap-2">
           <Button>Button</Button>
           <Button variant="outline">Outline</Button>
-          <span>Sum: {add(1, 2)}</span>
+          <span>Sum is: {add(2, 3)}</span>
         </div>
       </div>
     </div>
